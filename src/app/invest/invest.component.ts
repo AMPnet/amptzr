e54180bdb8133caf36c7ae9cb91fef746eac0943
theshasha <theshasha@theshashas-MacBook-Pro.local>
2021-07-26 14:13:54 +0200

    temp store

--- src/app/invest/invest.component.ts
+++ src/app/invest/invest.component.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core'
+import { BehaviorSubject } from 'rxjs'
 
 @Component({
   selector: 'app-invest',
@@ -7,6 +8,33 @@ import {ChangeDetectionStrategy, Component} from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InvestComponent {
+
+  project = new BehaviorSubject<InvestProjectModel>({
+    title: "Supercool Invest-O-Pia",
+    walletBalance: 500000,
+    roi: "8% - 11%",
+    description: "This is a small change, but a big move for us. 140 was an arbitrary choice based on the 160 character SMS limit. Proud of how thoughtful the team has been in solving a real problem people have when trying to tweet. And at the same time maintaining our brevity, speed, and essence!"
+  })
+  project$ = this.project.asObservable()
+
+  investmentAmount = new BehaviorSubject<number>(90000)
+  investmentAmount$ = this.investmentAmount.asObservable()
+
+  investmentState = new BehaviorSubject<InvestmentState>(InvestmentState.Editing)
+  investmentState$ = this.investmentState.asObservable()
+
   constructor() {
   }
+}
+
+interface InvestProjectModel {
+  title: string,
+  walletBalance: number,
+  roi: string, 
+  description: string
+}
+
+enum InvestmentState {
+  Editing,
+  InReview
 }
