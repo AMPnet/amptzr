7db3151aa5fad5713d78ff7fb3c882e333d0f847
Matija Pevec <mightymatth@users.noreply.github.com>
2021-07-28 15:01:17 +0200

    [Fixes SD-231] Money input field (#20)
    * started playing with listing transactions
    
    * added and modified ngx-currency directive

---
+++ src/app/shared/directives/currency-mask.directive.ts
@@ -0,0 +1,28 @@
+import {Directive, forwardRef, OnInit} from '@angular/core'
+import {CurrencyMaskConfig, CurrencyMaskDirective as NGXDirective, CurrencyMaskInputMode} from 'ngx-currency'
+import {NG_VALUE_ACCESSOR} from '@angular/forms'
+
+export const CURRENCYMASKDIRECTIVE_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => CurrencyMaskDirective),
+  multi: true,
+}
+
+@Directive({
+  selector: '[appCurrencyMask]',
+  providers: [CURRENCYMASKDIRECTIVE_VALUE_ACCESSOR],
+})
+export class CurrencyMaskDirective extends NGXDirective implements OnInit {
+  public optionsTemplate: CurrencyMaskConfig = {
+    align: 'right',
+    allowNegative: false,
+    allowZero: true,
+    decimal: ".",
+    precision: 2,
+    prefix: '$',
+    suffix: '',
+    thousands: ',',
+    nullable: false,
+    inputMode: CurrencyMaskInputMode.FINANCIAL,
+  }
+}
