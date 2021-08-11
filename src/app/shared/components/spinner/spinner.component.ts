4fc8c67542d049cdd2efc1e99822b5062af0b59d
Domagoj Latečki <domagoj.latecki@gmail.com>
2021-08-11 15:30:25 +0200

    SD-280: add animated overlay spinner

--- src/app/shared/components/spinner/spinner.component.ts
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -1,13 +1,18 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core'
+import {of} from "rxjs"
+import {delay} from "rxjs/operators"
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
-  styleUrls: ['./spinner.component.css'],
+  styleUrls: ['./spinner.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SpinnerComponent {
-  @Input() type: 'inline-xs' | 'inline-base' = 'inline-base'
+  @Input() type: 'inline-xs' | 'inline-base' | 'overlay' = 'inline-base'
+  delayedDisplay$ = of(true).pipe(
+    delay(300)
+  )
 
   constructor() {
   }
