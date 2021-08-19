224651f08530aad72054f4b3d58e09610e6e4e42
Matija Pevec <pevec@live.com>
2021-08-19 12:42:43 +0200

    fixed pinata

--- src/environments/env.base.ts
+++ src/environments/env.base.ts
@@ -11,8 +11,8 @@ export const environment = {
   },
   ipfs: {
     apiURL: postBuildEnv?.IPFS_API_URL || 'https://ipfs.infura.io:5001',
-    gatewayURL: postBuildEnv?.IPFS_API_URL || 'https://ipfs.io',
-    pinataApiURL: postBuildEnv?.IPFS_PINATA_API_URL || 'https://ampnet.mypinata.cloud',
+    gatewayURL: postBuildEnv?.IPFS_API_URL || 'https://ampnet.mypinata.cloud',
+    pinataApiURL: postBuildEnv?.IPFS_PINATA_API_URL || 'https://api.pinata.cloud',
   },
   backendURL: postBuildEnv?.BACKEND_URL || 'https://eth-staging.ampnet.io',
   fixed: {
