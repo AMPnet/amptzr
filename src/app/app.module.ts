ee111b8c0924232d0b179fec41c52fb303be484b
Domagoj Latečki <domagoj.latecki@gmail.com>
2021-08-19 14:40:47 +0200

    SD-287: add rich text editor and rich text view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -138,7 +138,43 @@ import {QuillModule} from 'ngx-quill'
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    QuillModule.forRoot(),
+    QuillModule.forRoot({
+      formats: [
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'blockquote',
+        'code-block',
+        'header',
+        'list',
+        'script',
+        'indent',
+        'direction',
+        'align',
+        'link',
+        'image',
+        'video'
+      ],
+      modules: {
+        toolbar: [
+          ['bold', 'italic', 'underline', 'strike'],
+          ['blockquote', 'code-block'],
+          [{'header': 1}, {'header': 2}],
+          [{'list': 'ordered'}, {'list': 'bullet'}],
+          [{'script': 'sub'}, {'script': 'super'}],
+          [{'indent': '-1'}, {'indent': '+1'}],
+          [{'direction': 'rtl'}],
+          // [{'size': ['small', false, 'large', 'huge']}], // disable font size changes
+          [{'header': [1, 2, false]}],
+          // [{'color': []}, {'background': []}], // disable color and background changes
+          // [{'font': ['']}], // disable font style changes
+          [{'align': []}],
+          ['clean'],
+          ['link', 'image', 'video'],
+        ]
+      }
+    }),
   ],
   providers: [
     PreferenceService,
