f66fb680356e9ddad340eb63bdcc1dcd9769af9b
Matija Pevec <pevec@live.com>
2021-09-07 21:03:52 +0200

    fixed isadmin; refactoring

--- src/app/shared/guards/admin.guard.ts
+++ src/app/shared/guards/admin.guard.ts
@@ -1,36 +1,21 @@
 import {Injectable} from '@angular/core'
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router'
-import {SessionQuery} from 'src/app/session/state/session.query'
-import {map} from 'rxjs/operators'
+import {map, take} from 'rxjs/operators'
 import {combineLatest, Observable} from 'rxjs'
-import {IssuerService} from '../services/blockchain/issuer.service'
-import {RouterService} from '../services/router.service'
-import {PreferenceQuery} from '../../preference/state/preference.query'
+import {UserService} from '../services/user.service'
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminGuard implements CanActivate {
-  constructor(private sessionQuery: SessionQuery,
-              private preferenceQuery: PreferenceQuery,
-              private issuerService: IssuerService,
-              private router: RouterService) {
+  constructor(private userService: UserService) {
   }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
     return combineLatest([
-      this.sessionQuery.address$,
-      this.issuerService.issuer$,
-    ]).pipe(
-      map(([address, issuer]) => {
-        if (!address || address !== issuer.owner) {
-          return this.router.router.createUrlTree([
-            `/${this.preferenceQuery.network.chainID}/${issuer.ansName}/offers`,
-          ])
-        }
-
-        return true
-      }),
+      this.userService.isAdmin$,
+    ]).pipe(take(1),
+      map(([isAdmin]) => isAdmin),
     )
   }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,14 +1,36 @@
 import {Injectable} from '@angular/core'
 import {SignerService} from './signer.service'
-import {switchMap} from 'rxjs/operators'
+import {distinctUntilChanged, map, shareReplay, switchMap} from 'rxjs/operators'
 import {BackendHttpClient} from './backend/backend-http-client.service'
+import {combineLatest, Observable} from 'rxjs'
+import {SessionQuery} from '../../session/state/session.query'
+import {IssuerService} from './blockchain/issuer.service'
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  isAdmin$: Observable<boolean>
+
   constructor(private signerService: SignerService,
+              private sessionQuery: SessionQuery,
+              private issuerService: IssuerService,
               private http: BackendHttpClient) {
+    this.isAdmin$ = combineLatest([
+      this.sessionQuery.isLoggedIn$,
+      this.sessionQuery.address$,
+      this.issuerService.issuer$,
+    ]).pipe(
+      map(([isLoggedIn, address, issuer]) => {
+        if (!isLoggedIn || !address) {
+          return false
+        }
+
+        return address.toLowerCase() === issuer.owner.toLowerCase()
+      }),
+      distinctUntilChanged(),
+      shareReplay(1),
+    )
   }
 
   logout() {
