5d8a08cfb4f642267c39a881c0e99a0d597b4543
Domagoj Latečki <domagoj.latecki@gmail.com>
2021-09-08 15:55:31 +0200

    SD-330: slice second part of campaign creration screen

--- src/app/admin/admin-campaign-new/admin-campaign-new.component.ts
+++ src/app/admin/admin-campaign-new/admin-campaign-new.component.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core'
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-admin-campaign-new',
@@ -7,7 +8,12 @@ import {ChangeDetectionStrategy, Component} from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdminCampaignNewComponent {
+  creationStep: 1 | 2 = 2
+  createForm: FormGroup
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.createForm = this.fb.group({
+      logo: [undefined, Validators.required],
+    })
   }
 }
