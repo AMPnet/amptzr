3594287d62a442402cdb39b461544195a8224cd6
Matija Pevec <pevec@live.com>
2021-09-14 01:35:17 +0200

    mod

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -3,4 +3,5 @@
 echo "Running envsub on env.template.js to env.js"
 envsub src/env.template.js dist/amptzr/env.js
 
+rm dist/amptzr/ngsw.json dist/amptzr/ngsw-worker.js
 ngsw-config dist/amptzr ngsw-config.json
