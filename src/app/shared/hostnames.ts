f20cc7f65f2dbc8e39d6c5e970f978e2395b7402
Matija Pevec <pevec@live.com>
2021-09-28 12:07:49 +0200

    updated hostnames

--- src/app/shared/hostnames.ts
+++ src/app/shared/hostnames.ts
@@ -1,11 +1,7 @@
-import {MaticNetwork, MumbaiNetwork, Network} from './networks'
+import {MumbaiNetwork, Network} from './networks'
 import {getWindow} from './utils/browser'
 
 export const Hostnames: { [host in string]: Host } = {
-  'token.mining4mining.io': {
-    network: MaticNetwork,
-    issuerAddress: '0xA240868AF33f6E44d85654bBE1CA73B9b9eA7C3d',
-  },
   'woz.eugen.work': {
     network: MumbaiNetwork,
     issuerAddress: '0x75C7193E8e0C8179A0cb2FF190Bc8E4681ffA8c7',
