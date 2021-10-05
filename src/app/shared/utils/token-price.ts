d2ce13feadd1ec13b4ac233341554cfe281cf96e
Domagoj Latečki <domagoj.latecki@gmail.com>
2021-10-05 15:29:35 +0200

    SD-415: disable investment for non-funded campaigns

--- src/app/shared/utils/token-price.ts
+++ src/app/shared/utils/token-price.ts
@@ -10,11 +10,14 @@
  * If the preferred token price is `0.000567` (USDC), the token price will be stored as `5` in the contract.
  */
 export class TokenPrice {
+  static readonly decimals = 4
+  static readonly scale = 10 ** TokenPrice.decimals
+
   static format(value: number): number {
-    return Math.floor(Number(value) * 10_000)
+    return Math.floor(Number(value) * TokenPrice.scale)
   }
 
   static parse(value: number): number {
-    return Number(value) / 10_000
+    return Number(value) / TokenPrice.scale
   }
 }
