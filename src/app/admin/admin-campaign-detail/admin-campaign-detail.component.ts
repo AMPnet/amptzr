f62955c613ec2c3c4bfa1e3cce37504f980eb7f1
Domagoj Latečki <domagoj.latecki@gmail.com>
2021-10-08 14:10:41 +0200

    SD-400: revert max investment display on admin campaign detail

--- src/app/admin/admin-campaign-detail/admin-campaign-detail.component.ts
+++ src/app/admin/admin-campaign-detail/admin-campaign-detail.component.ts
@@ -164,9 +164,4 @@ export class AdminCampaignDetailComponent {
       )
     }
   }
-
-  campaignHasMaxInvestment(asset: CommonAssetWithInfo, campaignStats: CampaignStats) {
-    const maxValue = this.stablecoinService.format(asset.totalSupply, 18) * campaignStats.tokenPrice
-    return campaignStats.userMax < maxValue
-  }
 }
