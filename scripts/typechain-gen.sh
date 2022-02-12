903e5214264bcf0654f3a30cc588013925a0fb06
Matija Pevec <pevec@live.com>
2022-02-12 14:27:40 +0100

    added token prototype submodule; new contract management; adapted code to contracts

--- scripts/typechain-gen.sh
+++ scripts/typechain-gen.sh
@@ -3,8 +3,11 @@
 rm -rf abis
 rm -rf types/ethers-contracts
 
-cp -r node_modules/tokenizer-prototype/artifacts/contracts abis
-cp -r node_modules/tokenizer-prototype/artifacts/@openzeppelin abis/@openzeppelin
+cp -r deps/tokenizer-prototype/contracts contracts/tokenizer-prototype
+hardhat compile
+
+cp -r artifacts/contracts abis
+cp -r artifacts/@openzeppelin abis/@openzeppelin
 find abis -name '*.dbg.json' -delete
 
 typechain --target=ethers-v5 'abis/**/*.json'
