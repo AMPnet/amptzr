e951a4a137f9d52404cca42301a901de37eb58ea
Matija Pevec <pevec@live.com>
2022-03-01 12:02:35 +0100

    refactor home screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core'
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core'
 import {combineLatest, Observable, of} from 'rxjs'
 import {WithStatus, withStatus} from '../shared/utils/observables'
 import {map, switchMap} from 'rxjs/operators'
 import {RouterService} from '../shared/services/router.service'
 import {IssuerService, IssuerWithInfo} from '../shared/services/blockchain/issuer/issuer.service'
 import {QueryService} from '../shared/services/blockchain/query.service'
+import {ChainID} from '../shared/networks'
+import {PreferenceQuery} from '../preference/state/preference.query'
+import {PreferenceService} from '../preference/state/preference.service'
+import {SessionQuery} from '../session/state/session.query'
 
 @Component({
   selector: 'app-home',
@@ -12,24 +16,73 @@ import {QueryService} from '../shared/services/blockchain/query.service'
   styleUrls: ['./home.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
-  issuers$: Observable<WithStatus<IssuerItem[]>> = this.queryService.issuers$.pipe(
-    switchMap(issuers => withStatus(
-        issuers.length === 0 ? of([]) : combineLatest(
-          issuers.map(issuer => this.issuerService.getIssuerInfo(issuer.issuer).pipe(
-            map(i => ({mappedName: issuer.mappedName, issuer: i})),
-          ))),
+export class HomeComponent implements OnInit {
+  featuredMappings: { [key in ChainID]?: string[] } = {
+    [ChainID.MATIC_MAINNET]: [
+      '0x950c8472c5bb8a10e111646a3de7fd226306b5cf',
+    ],
+    [ChainID.MUMBAI_TESTNET]: [
+      '0x5d1800560a29ba3e8130D2B48Bd4c510a506DD62',
+    ],
+  }
+
+  networkAndAddress$ = combineLatest([
+    this.preferenceQuery.network$,
+    this.preferenceQuery.address$,
+  ])
+
+  featuredAddrs$: Observable<string[]> = this.networkAndAddress$.pipe(
+    map(([network, _address]) => this.featuredMappings[network.chainID] || []),
+  )
+
+  featuredIssuers$: Observable<WithStatus<IssuerItem[]>> = this.featuredAddrs$.pipe(
+    switchMap(issuersAddrs => withStatus(
+        issuersAddrs.length === 0 ? of([]) : combineLatest(
+          issuersAddrs.map(address => this.queryService.getIssuerForAddress(address).pipe(
+            switchMap(issuer => this.issuerService.getIssuerInfo(issuer.issuer).pipe(
+              map(i => ({mappedName: issuer.mappedName, issuer: i})),
+            )),
+          )),
+        ),
       ),
     ),
   )
 
+  myIssuers$: Observable<WithStatus<IssuerItem[]>> = this.networkAndAddress$.pipe(
+    switchMap(() => withStatus(
+      this.queryService.issuers$.pipe(
+        map(items => items.filter(item =>
+          item.issuer.owner.toLowerCase() === this.preferenceQuery.getValue().address.toLowerCase()),
+        ),
+        switchMap(issuers => issuers.length === 0 ? of([]) : combineLatest(
+          issuers.map(issuer => this.issuerService.getIssuerInfo(issuer.issuer).pipe(
+            map(i => ({mappedName: issuer.mappedName, issuer: i})),
+          ))),
+        ),
+      ),
+    )),
+  )
+
+  isLoggedIn$ = this.sessionQuery.isLoggedIn$
+
   constructor(private issuerService: IssuerService,
               private router: RouterService,
+              private preferenceQuery: PreferenceQuery,
+              private sessionQuery: SessionQuery,
+              private preferenceService: PreferenceService,
               private queryService: QueryService) {
   }
 
+  ngOnInit() {
+    this.preferenceService.resetIssuer()
+  }
+
   openIssuer(issuer: IssuerItem) {
-    this.router.navigateIssuer(issuer.mappedName || issuer.issuer.contractAddress)
+    return this.router.navigateIssuer(issuer.mappedName || issuer.issuer.contractAddress)
+  }
+
+  openCreateNewIssuer() {
+    return this.router.navigateNetwork(['/admin/issuers/new'])
   }
 }
 
--- src/app/shared/services/crisp.service.ts
+++ src/app/shared/services/crisp.service.ts
@@ -9,121 +9,124 @@ import {BackendUserService} from './backend/backend-user.service'
 import {switchMapTap} from '../utils/observables'
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class CrispService {
-  private crispSub = new BehaviorSubject<boolean>(false)
+    private crispSub = new BehaviorSubject<boolean>(false)
 
-  isAvailable$: Observable<boolean>
-  keepShown$: Observable<unknown>
+    isAvailable$: Observable<boolean>
+    keepShown$: Observable<unknown>
 
-  constructor(@Inject(DOCUMENT) private document: any,
-              private preferenceQuery: PreferenceQuery,
-              private backendUserService: BackendUserService,
-              private issuerService: IssuerService) {
-    const crispWebsiteId$ = combineLatest([this.issuerService.issuer$]).pipe(
-      map(([issuer]) => issuer.infoData.crispWebsiteId),
-      distinctUntilChanged(),
-      tap(() => this.crispSub.next(false)),
-      shareReplay(1),
-    )
+    constructor(@Inject(DOCUMENT) private document: any,
+                private preferenceQuery: PreferenceQuery,
+                private backendUserService: BackendUserService,
+                private issuerService: IssuerService) {
+        const crispWebsiteId$ = combineLatest([
+            this.preferenceQuery.issuer$,
+            this.issuerService.issuer$,
+        ]).pipe(
+            map(([prefIssuer, issuer]) => !prefIssuer.address ? undefined : issuer.infoData.crispWebsiteId),
+            distinctUntilChanged(),
+            tap(() => this.crispSub.next(false)),
+            shareReplay(1),
+        )
 
-    this.isAvailable$ = this.crispSub.asObservable()
+        this.isAvailable$ = this.crispSub.asObservable()
 
-    this.keepShown$ = crispWebsiteId$.pipe(
-      switchMap(websiteId => {
-        return !!websiteId ? this.loadScript(websiteId).pipe(
-          tap(isLoaded => this.crispSub.next(isLoaded)),
-          shareReplay(1),
-        ) : of(false)
-      }),
-      shareReplay({bufferSize: 1, refCount: true}),
-      tap({
-        next: shouldShow => {
-          if (shouldShow) this.chatShow()
-        },
-        finalize: () => {
-          this.chatHide()
-        },
-      }),
-    )
+        this.keepShown$ = crispWebsiteId$.pipe(
+            switchMap(websiteId => {
+                return !!websiteId ? this.loadScript(websiteId).pipe(
+                    shareReplay(1),
+                ) : of(false)
+            }),
+            tap(shouldShow => this.crispSub.next(shouldShow)),
+            shareReplay({bufferSize: 1, refCount: true}),
+            tap({
+                next: shouldShow => {
+                    shouldShow ? this.chatShow() : this.chatHide()
+                },
+                finalize: () => {
+                    this.chatHide()
+                },
+            }),
+        )
 
-    combineLatest([
-      this.crispSub.asObservable(),
-      this.preferenceQuery.isBackendAuthorized$,
-      this.preferenceQuery.address$,
-    ]).pipe(
-      tap(([isCrispAvailable, isBackendAuthorized, address]) => {
-        if (!isCrispAvailable) return
+        combineLatest([
+            this.crispSub.asObservable(),
+            this.preferenceQuery.isBackendAuthorized$,
+            this.preferenceQuery.address$,
+        ]).pipe(
+            tap(([isCrispAvailable, isBackendAuthorized, address]) => {
+                if (!isCrispAvailable) return
 
-        this.setSessionData({
-          address: !!address ? `"${address}"` : '',
-          authProvider: this.preferenceQuery.getValue().authProvider || '',
+                this.setSessionData({
+                    address: !!address ? `"${address}"` : '',
+                    authProvider: this.preferenceQuery.getValue().authProvider || '',
+                })
+
+                if (isBackendAuthorized) {
+                    this.backendUserService.getUser().pipe(
+                        tap(user => {
+                            if (!!user.email) this.setUserEmail(user.email)
+                        }),
+                    ).subscribe()
+                }
+            }),
+        ).subscribe()
+    }
+
+    chatShow() { // visible
+        this.$crisp?.push(["do", "chat:show"])
+    }
+
+    chatHide() { // invisible
+        this.$crisp?.push(["do", "chat:hide"])
+    }
+
+    chatOpen() {
+        this.$crisp?.push(["do", "chat:open"])
+    }
+
+    setUserEmail(email: string) {
+        this.$crisp?.push(["set", "user:email", email])
+    }
+
+    setSessionData(data: object) {
+        this.$crisp?.push(["set", "session:data", [[...Object.entries(data)]]])
+    }
+
+    private get $crisp(): any | undefined {
+        return getWindow().$crisp
+    }
+
+    private loadScript(crispWebsiteId: string): Observable<boolean> {
+        if (this.crispSub.value) return of(true)
+
+        const loadScript$ = new Observable<boolean>(subscriber => {
+            const head = this.document.getElementsByTagName('head')[0]
+
+            getWindow().$crisp = []
+            getWindow().CRISP_WEBSITE_ID = crispWebsiteId
+            const script: HTMLScriptElement = this.document.createElement('script')
+            script.src = 'https://client.crisp.chat/l.js'
+            script.async = true
+
+            script.onload = () => {
+                subscriber.next(true)
+                subscriber.complete()
+            }
+            script.onerror = () => {
+                subscriber.error(false)
+                subscriber.complete()
+            }
+
+            head.appendChild(script)
         })
 
-        if (isBackendAuthorized) {
-          this.backendUserService.getUser().pipe(
-            tap(user => {
-              if (!!user.email) this.setUserEmail(user.email)
-            }),
-          ).subscribe()
-        }
-      }),
-    ).subscribe()
-  }
-
-  chatShow() { // visible
-    this.$crisp?.push(["do", "chat:show"])
-  }
-
-  chatHide() { // invisible
-    this.$crisp?.push(["do", "chat:hide"])
-  }
-
-  chatOpen() {
-    this.$crisp?.push(["do", "chat:open"])
-  }
-
-  setUserEmail(email: string) {
-    this.$crisp?.push(["set", "user:email", email])
-  }
-
-  setSessionData(data: object) {
-    this.$crisp?.push(["set", "session:data", [[...Object.entries(data)]]])
-  }
-
-  private get $crisp(): any | undefined {
-    return getWindow().$crisp
-  }
-
-  private loadScript(crispWebsiteId: string): Observable<boolean> {
-    if (this.crispSub.value) return of(true)
-
-    const loadScript$ = new Observable<boolean>(subscriber => {
-      const head = this.document.getElementsByTagName('head')[0]
-
-      getWindow().$crisp = []
-      getWindow().CRISP_WEBSITE_ID = crispWebsiteId
-      const script: HTMLScriptElement = this.document.createElement('script')
-      script.src = 'https://client.crisp.chat/l.js'
-      script.async = true
-
-      script.onload = () => {
-        subscriber.next(true)
-        subscriber.complete()
-      }
-      script.onerror = () => {
-        subscriber.error(false)
-        subscriber.complete()
-      }
-
-      head.appendChild(script)
-    })
-
-    return loadScript$.pipe(
-      switchMapTap(() => fromEventPattern(
-        handler => getWindow().CRISP_READY_TRIGGER = handler(),
-      ).pipe(take(1))),
-    )
-  }
+        return loadScript$.pipe(
+            switchMapTap(() => fromEventPattern(
+                handler => getWindow().CRISP_READY_TRIGGER = handler(),
+            ).pipe(take(1))),
+        )
+    }
 }
