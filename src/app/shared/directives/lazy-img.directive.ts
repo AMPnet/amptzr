6cbb2f188504df2b85f407ede5177fda3871c9a9
Matija Pevec <pevec@live.com>
2022-05-02 16:23:45 +0200

    add opacity switch on all images on load

--- src/app/shared/directives/lazy-img.directive.ts
+++ src/app/shared/directives/lazy-img.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core'
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core'
 
 // eslint-disable-next-line @angular-eslint/directive-selector
 @Directive({selector: 'img'})
@@ -8,5 +8,16 @@ export class LazyImgDirective {
     if ('loading' in HTMLImageElement.prototype) {
       this.renderer.setAttribute(this.element.nativeElement, 'loading', 'lazy')
     }
+    this.renderer.setStyle(this.element.nativeElement, 'opacity', '0')
+  }
+
+  @HostListener('load')
+  onLoad() {
+    this.renderer.setStyle(this.element.nativeElement, 'opacity', '1')
+  }
+
+  @HostListener('error')
+  onError() {
+    this.renderer.setStyle(this.element.nativeElement, 'opacity', '1')
   }
 }
