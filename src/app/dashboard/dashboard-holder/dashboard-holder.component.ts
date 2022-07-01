d2973ce9cefc8a9a54432379fd84e4779fb1fac0
theshasha <theshasha@theshashas-MacBook-Pro.local>
2022-07-01 17:25:53 +0200

    fixed rxjs

--- src/app/dashboard/dashboard-holder/dashboard-holder.component.ts
+++ src/app/dashboard/dashboard-holder/dashboard-holder.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core'
 import { Router } from '@angular/router'
-import { map, Observable, of, pipe, switchMap } from 'rxjs'
+import { BehaviorSubject, map, Observable, of, pipe, switchMap, tap } from 'rxjs'
 import { PreferenceQuery } from 'src/app/preference/state/preference.query'
 import { BackendHttpClient } from 'src/app/shared/services/backend/backend-http-client.service'
 import { JwtTokenService } from 'src/app/shared/services/backend/jwt-token.service'
 import { ProjectService } from 'src/app/shared/services/backend/project.service'
 import { IssuerService, IssuerWithInfo } from 'src/app/shared/services/blockchain/issuer/issuer.service'
+import { DialogService } from 'src/app/shared/services/dialog.service'
 import { RouterService } from 'src/app/shared/services/router.service'
 import { withStatus } from 'src/app/shared/utils/observables'
 
@@ -21,21 +22,25 @@ export class DashboardHolderComponent {
   issuer$ = this.issuerService.issuer$
   network$ = this.preferenceQuery.network$
   isBackendAuthorized$ = this.preferenceQuery.isBackendAuthorized$
-  apiKey$ = this.isBackendAuthorized$.pipe(
-    switchMap(isAuthorized => isAuthorized ? this.fetchApiKey() : of())
+
+  refreshSnapshotSub = new BehaviorSubject<void>(undefined)
+
+
+  apiKey$ = this.refreshSnapshotSub.asObservable().pipe(
+    switchMap(() => this.isBackendAuthorized$),
+    switchMap(isAuthorized => isAuthorized ? this.fetchApiKey() : of(undefined))
   )
 
   constructor(private router: RouterService,
     private issuerService: IssuerService,
-    private jwtService: JwtTokenService,
     private projectService: ProjectService,
+    private dialogService: DialogService,
     private http: BackendHttpClient,
     private preferenceQuery: PreferenceQuery) { }
 
   sidebarItemClicked(index: number) {
     this.navbarSelectedIndex = index
     this.changeRoute()
-
   }
 
   authorize() {
@@ -44,7 +49,12 @@ export class DashboardHolderComponent {
 
   createApiKey() {
     return this.projectService.getProjectIdByChainAndAddress().pipe(
-      switchMap(project => this.projectService.createApiKey(project.id))
+      switchMap(project => this.projectService.createApiKey(project.id)),
+      switchMap(() => this.dialogService.info({
+        title: 'Success',
+        message: `Your API Key wass successfully created.`,
+        cancelable: false,
+      })), tap(() => this.refreshSnapshotSub.next())
     )
   }
 
